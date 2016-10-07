b029312b34e8a2869ab07bf738291825af0ca752
JaneCheng00 <ziguinihao@aliyun.com>
2016-10-07 23:21:54 +0800

    添加 navigator

--- index.android.js
+++ index.android.js
@@ -6,30 +6,66 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator
 } from 'react-native';
-import Index from './src/Index';
+import  Login from './src/containers/Login';
+import Main from './src/containers/Main';
+import CheckInList from './src/containers/CheckInList';
 
 class RNDemo extends Component {
+
+    _renderScene(route, navigator) {
+        console.log(route.id);
+        switch (route.id) {
+            case 'login':
+                return (<Login navigator={navigator}
+                               onBack={() => {
+                                   if (route.index > 0) {
+                                       navigator.pop();
+                                   }
+                               }}/>);
+            case 'main':
+                return (<Main navigator={navigator}
+                              onBack={() => {
+                                  if (route.index > 0) {
+                                      navigator.pop();
+                                  }
+                              }}/>);
+            case 'checkList':
+                return (<CheckInList navigator={navigator}
+                                     onBack={() => {
+                                         if (route.index > 0) {
+                                             navigator.pop();
+                                         }
+                                     }}/>);
+            default:
+                return (<Text>默认页面</Text>);
+                break
+        }
+    }
+
   render() {
+      console.log("android 版本");
+
     return (
-      <View style={styles.container}>
-        <Index/>
-      </View>
+        <Navigator
+            style={styles.container}
+            initialRoute={{name: '登录页面', index: 0, id: 'login'}}
+            renderScene={this._renderScene}
+        />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
