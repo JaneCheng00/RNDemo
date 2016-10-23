610def21a78ed83c3569bded0bbb731b92a93761
JaneCheng00 <ziguinihao@aliyun.com>
2016-10-23 22:41:35 +0800

    增加 action

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,7 @@
+/**
+ * Created by apple on 16/10/23.
+ * 管理所有的 action types
+ */
+export const LOGIN_IN_INIT = 'LOGIN_IN_INIT'; // 初始状态
+export const LOGIN_IN_DOING = 'LOGIN_IN_DOING'; // 正在登录
+export const LOGIN_IN_DONE = 'LOGIN_IN_DONE'; // 登录完成
