ee77fb5226d84794e66965375874250bac8b465c
JaneCheng00 <ziguinihao@aliyun.com>
2016-10-23 22:42:56 +0800

    使用 redux

--- src/Root.js
+++ src/Root.js
@@ -6,12 +6,18 @@
 import React, {Component} from 'react';
 
 import {Provider} from 'react-redux';
+import configureStore from './store/ConfigureStore';
+
 import RNDemoNavigator from './RNDemoNavigator';
+
+const store = configureStore();
 
 export default class Root extends Component {
     render() {
         return (
-            <RNDemoNavigator/>
+            <Provider store={store}>
+                <RNDemoNavigator/>
+            </Provider>
         );
     }
 }
